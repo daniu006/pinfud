0c4c4f98073a38c03565ad2c250d169135cda0f1
daniu006 <dpabad@sudamericano.edu.ec>
2025-12-07 16:46:15 -0500

    grid

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,20 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule   
+  ]
 })
-export class HomePage implements OnInit {
+export class HomePage {
 
-  constructor() { }
+  activeTab: string = 'home';
+  selectedCategory: number = 1;
 
-  ngOnInit() {
+  imageModalOpen: boolean = false;
+  selectedItem: any = null;
+
+  categories = [
+    { id: 1, name: 'Para ti'},
+    { id: 2, name: 'Costa' },
+    { id: 3, name: 'Sierra' },
+    { id: 4, name: 'Oriente' },
+    { id: 5, name: 'Galapagos' },
+   
+  ];
+
+  foodItems = [
+    { id: 1, title: 'Pizza Pep', price: '$12.99', rating: 4.8, height: 240, image: '🍕' },
+    { id: 2, title: 'Burguer King', price: '$8.50', rating: 4.6, height: 280, image: '🍔' },
+    { id: 3, title: 'Sushi Rolls', price: '$15.20', rating: 4.9, height: 210, image: '🍣' },
+    { id: 4, title: 'Cupcakes', price: '$6.00', rating: 4.5, height: 260, image: '🧁' },
+    { id: 5, title: 'Pollo BBQ', price: '$10.50', rating: 4.7, height: 230, image: '🍗' },
+  ];
+
+  setActiveTab(tab: string) {
+    this.activeTab = tab;
   }
 
+  selectCategory(categoryId: number) {
+    this.selectedCategory = categoryId;
+  }
+
+  openItem(item: any) {
+    this.selectedItem = item;
+    this.imageModalOpen = true;
+  }
 }
